d3d788803dcdbbbdf28067e291d26571edbadb25
jvo <mrjhonyvidal@gmail.com>
2024-03-19 19:06:03 +0000

    Set ConfigService and use it in MongoMondule receiving the MONGODB_URI property

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+        imports: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+            uri: configService.get('MONGO_URI')
+        }),
+        inject: [ConfigService]
+    }),
+  ],
+})
 export class DatabaseModule {}
